8c1dfed045fdb3807e8e6142c96e0b187deafde3
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2019-08-01 14:21:00 -0600

    Update/Move Template Plugin

--- f4se/f4se_plugins/TemplatePlugin/Template/F4SE Plugin Template/Config.h
+++ f4se/f4se_plugins/TemplatePlugin/Template/F4SE Plugin Template/Config.h
@@ -6,8 +6,8 @@
 //-----------------------
 #define PLUGIN_VERSION              1
 #define PLUGIN_VERSION_STRING       "1.0.0"
-#define PLUGIN_NAME_SHORT           "$projectname$"
-#define PLUGIN_NAME_LONG            "$projectname$"
+#define PLUGIN_NAME_SHORT           "$safeprojectname$"
+#define PLUGIN_NAME_LONG            "$safeprojectname$"
 #define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
 #define MINIMUM_RUNTIME_VERSION     RUNTIME_VERSION_1_10_138
 #define COMPATIBLE(runtimeVersion)  (runtimeVersion == SUPPORTED_RUNTIME_VERSION)
--- f4se/f4se_plugins/TemplatePlugin/Template/F4SE Plugin Template/main.cpp
+++ f4se/f4se_plugins/TemplatePlugin/Template/F4SE Plugin Template/main.cpp
@@ -1,4 +1,5 @@
-#include "config.h"
+#include "Config.h"
+
 #include "f4se_globals/Globals.h"
 
 #include "f4se/PluginAPI.h"
